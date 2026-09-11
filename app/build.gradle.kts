53fe76a6a8893d74cb3569cd6286154ef7511d2b
Tange <TangeTiw@outlook.com>
2026-09-11 19:42:54 +0800

    chore: bump version to 1.2.3 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.xingheyuzhuan.shiguangschedule"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
