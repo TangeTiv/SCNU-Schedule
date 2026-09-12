b1b230cfb0cd62c1801e2e279d9a1b75f2cb7866
Tange <TangeTiw@outlook.com>
2026-09-12 22:50:30 +0800

    chore: bump version to 1.5.0 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.xingheyuzhuan.shiguangschedule"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.4.1"
+        versionCode = 11
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
