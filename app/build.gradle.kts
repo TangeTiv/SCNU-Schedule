821e7bbc53daef4bdd686c3b77d5ae24a3a81a1d
Tange <TangeTiw@outlook.com>
2026-09-23 23:04:45 +0800

    chore: bump version to 1.8.0 (versionCode 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.xingheyuzhuan.shiguangschedule"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.7.1"
+        versionCode = 15
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
